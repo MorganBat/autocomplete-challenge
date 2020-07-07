59b5742e3977b56708097ca8a2ce586525845f0f
Morgan <morgan.batterham@gmail.com>
2020-07-07 15:40:20 +1000

    done

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Autocomplete = ({possibleSearchResults}) => {
+
+    const[searchValue, setSearchValue] = useState('')
+
     return (
         <div>
-            Autocomplete Here
+            <input
+                type="text"
+                label="country"
+                value={searchValue}
+                onChange={e => setSearchValue(e.target.value)}
+            />
+            {searchValue && possibleSearchResults.filter(r => r.includes(searchValue)).map(t => <li>{t}</li>)}        
         </div>
     )
 }
